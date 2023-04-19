0abd76ab41f29e2b3630f324bbee2b9bdff62fe4
AshrafUzzaman04 <ashraf.uzzaman04082004@gmail.com>
2023-04-20 03:35:30 +0600

    some design

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,24 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+
+<!-- jquery cdn -->
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
 <!-- ion icons script -->
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
 <!-- register -->
-<script src="./assets/js/index.js?<?= time(); ?>"></script>
+<script src="./assets/js/index.js?<?php echo time(); ?>"></script>
+
 <!-- gsap link -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js" integrity="sha512-f8mwTB+Bs8a5c46DEm7HQLcJuHMBaH/UFlcgyetMqqkvTcYg4g5VXsYR71b3qC82lZytjNYvBj2pf0VekA9/FQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="assets/js/gsap.js?<?= time(); ?>"></script>
+<script src="assets/js/gsap.js?<?php echo time(); ?>"></script>
+
 <!-- navigaiton -->
-<script src="assets/js/nav.js?<?= time(); ?>"></script>
+<script src="assets/js/nav.js?<?php echo time(); ?>"></script>
+
 </body>
 
 </html>
--- updateProfile.php
+++ updateProfile.php
@@ -14,8 +14,130 @@ include_once("./includes/nav.php");
                 </div>
                 <div class="bg"></div>
             </div>
-            <div class="form_body_input bg-danger">
-                <input type="text">
+            <div class="form_body_input">
+
+                <!-- student name -->
+                <div class="grid">
+                    <div class="wave-group">
+                        <input required="" type="text" class="input">
+                        <span class="bar"></span>
+                        <label class="label">
+                            <span class="label-char" style="--index: 0">N</span>
+                            <span class="label-char" style="--index: 1">a</span>
+                            <span class="label-char" style="--index: 2">m</span>
+                            <span class="label-char" style="--index: 3">e</span>
+                        </label>
+                    </div>
+
+                    <!-- student mobile -->
+                    <div class="wave-group">
+                        <input required="" type="text" class="input">
+                        <span class="bar"></span>
+                        <label class="label">
+                            <span class="label-char" style="--index: 0">M</span>
+                            <span class="label-char" style="--index: 1">o</span>
+                            <span class="label-char" style="--index: 2">b</span>
+                            <span class="label-char" style="--index: 3">i</span>
+                            <span class="label-char" style="--index: 4">l</span>
+                            <span class="label-char" style="--index: 5">e</span>
+                        </label>
+                    </div>
+
+                    <!-- student email -->
+                    <div class="wave-group">
+                        <input required="" type="text" class="input">
+                        <span class="bar"></span>
+                        <label class="label">
+                            <span class="label-char" style="--index: 0">E</span>
+                            <span class="label-char" style="--index: 1">m</span>
+                            <span class="label-char" style="--index: 2">a</span>
+                            <span class="label-char" style="--index: 3">i</span>
+                            <span class="label-char" style="--index: 4">l</span>
+                        </label>
+                    </div>
+                </div>
+
+
+                <!-- student gender -->
+                <div class="radio_gender mt-3 mb-1">
+                    <div class="radio-container">
+                        <h3 class="text-secondary">Gender</h3>
+                        <div class="radio-wrapper">
+                            <label class="radio-button">
+                                <input type="radio" name="gender" value="Male">
+                                <span class="radio-checkmark"></span>
+                                <span class="radio-label">Male</span>
+                            </label>
+                        </div>
+
+                        <div class="radio-wrapper">
+                            <label class="radio-button">
+                                <input type="radio" name="gender" id="option2" value="Female">
+                                <span class="radio-checkmark"></span>
+                                <span class="radio-label">Female</span>
+                            </label>
+                        </div>
+
+                        <div class="radio-wrapper">
+                            <label class="radio-button">
+                                <input type="radio" name="gender" id="option3" value="Other">
+                                <span class="radio-checkmark"></span>
+                                <span class="radio-label">Other</span>
+                            </label>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <!-- student location -->
+                <div class="your_location">
+                    <h3 class="text-secondary">Location</h3>
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected> -- Division -- </option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected> -- District -- </option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+
+                <!-- student subject -->
+                <div class="subject_check mt-1">
+                    <h3 class="text-secondary">Your Subject</h3>
+
+                    <!-- checkbox one -->
+                    <div class="checkbox-wrapper-12">
+                        <div class="cbx">
+                            <input id="cbx-12" type="checkbox">
+                            <label for="cbx-12"></label>
+                            <svg width="15" height="14" viewBox="0 0 15 14" fill="none">
+                                <path d="M2 8.36364L6.23077 12L13 2"></path>
+                            </svg>
+                            <span>Web Development</span>
+                        </div>
+                    </div>
+
+                    <!-- checkbox 2 -->
+                    <div class="checkbox-wrapper-12">
+                        <div class="cbx">
+                            <input id="cbx-12" type="checkbox">
+                            <label for="cbx-12"></label>
+                            <svg width="15" height="14" viewBox="0 0 15 14" fill="none">
+                                <path d="M2 8.36364L6.23077 12L13 2"></path>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <button>Update</button>
+                </div>
             </div>
         </form>
     </div>
